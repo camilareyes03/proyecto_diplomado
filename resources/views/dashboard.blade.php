a4a544fb30f2c3fe0882dc100593b942afe29eb3
TillWurner <85416653+TillWurner@users.noreply.github.com>
2023-10-15 16:35:32 -0400

    p2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,2 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
-
-@section('content_header')
-@stop
-
-@section('content')
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Bienvenidos') }}
-        </h2>
-    </x-slot>
-
-</x-app-layout>
-@stop
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
